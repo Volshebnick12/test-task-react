e926ef02428960cadeb5f01d13787807a2e31f39
Volshebnick12 <j_a_shurik@bk.ru>
2022-08-17 22:18:00 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import ThirdBlock from './components/  ThirdBlock/  ThirdBlock';
 import FirstBlock from './components/FirstBlock/FirstBlock';
 import SecondBlock from './components/SecondBlock/SecondBlock';
-
+import ThirdBlock from './components/ThirdBlock/ThirdBlock';
 
 function App() {
   return (
